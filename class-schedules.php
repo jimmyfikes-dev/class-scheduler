5bd25c6a2daf3c34a7f583f608a80a1e337876af
jimmyfikes-dev <jimmfikes@gmail.com>
2019-02-09 13:55:18 -0800

    commit

---
+++ class-schedules.php
@@ -0,0 +1,11 @@
+<?php 
+
+class student {
+    public $username = $_POST['username'];
+    public $email = $_POST['email'];
+    public $subject = $_POST['subject'];
+    public $topic = $_POST['topic'];
+    public $timeslot = $_POST['timeslot'];
+}
+
+?>
